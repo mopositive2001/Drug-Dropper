6170be53a7a4e2d9184dd4806473318e6771d599
mopositive2001 <55818984+mopositive2001@users.noreply.github.com>
2022-09-25 07:47:00 -0500

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    const scanned=true;
+
+    function animation1(){
+        
+        //document.getElementById("touch-pad").innerHTML="Hello Mickey Mouse, please enter you pin number";
+        //document.getElementById("blue").innerHTML="Blue (9)";
+        //document.getElementById("red").innerHTML="Blue (8)";
+    }
+
+    if(scanned){
+        animation1();
+    }
+})
